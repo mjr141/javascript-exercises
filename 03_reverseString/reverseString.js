eeaf696b204d6778c1c1967d63cc77a10f77f910
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-11 11:01:24 +0100

    complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(stringToReverse) {
+  let newString = '';
+  const negativeLength = stringToReverse.length * -1; // we use this as we count *backwards* using a negative index
+  for (x = -1; x >= negativeLength; x--) {
+    newString += stringToReverse.at(x); // .at method can use a negative index to count from the end of the string
+  }
+  return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
