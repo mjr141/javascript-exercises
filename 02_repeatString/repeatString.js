f8e040a5ae115ef3a3d556f2dceae01bb991c701
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-11 10:44:57 +0100

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfRepeats) {
+  if (numberOfRepeats < 0) {
+    return 'ERROR';
+  } else {
+    let completeString = '';
+    for (x = 0; x < numberOfRepeats; x++) {
+      completeString = completeString + stringToRepeat;
+    }
+    return completeString;
+  }
 };
 
 // Do not edit below this line
