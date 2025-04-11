1eb3cb545bb0377e0b97f44bf9991fa18e077d67
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-11 11:44:29 +0100

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argsToRemove) {
+  for (arg of argsToRemove) {
+    while (array.includes(arg)) {
+      let position = array.indexOf(arg);
+      array = array.toSpliced(position, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
