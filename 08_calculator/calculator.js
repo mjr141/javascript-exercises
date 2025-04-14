019d77d40a14dc74df159241b33b627c26ba1785
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-14 13:44:19 +0100

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce ((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  for (i = a; i > 1; i--) {
+    result *= i;
+  }
 };
 
 // Do not edit below this line
