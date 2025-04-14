56fefaba46ac7cf0b52ec6769413c242fe1bd35d
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-14 13:13:14 +0100

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempC) {
+  const factor = 5/9;
+  let tempF = ((tempC - 32)*factor);
+  tempF = (Math.ceil(tempF*10) / 10); // convert to 1 decimal place
+  return tempF;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempF) {
+  const factor = 9/5;
+  let tempC = (tempF*factor + 32);
+  tempC = (Math.ceil(tempC*10) / 10);
+  return tempC;
 };
 
 // Do not edit below this line
