05e31febe95f69b2c57e05aaee285231b898fac0
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-14 12:45:06 +0100

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+  let sum = 0;
+  
+  for (arg of arguments) {
+    if (arg < 0 || !Number.isInteger(arg)) {
+      return 'ERROR';
+    }
+  }
 
-};
+  // swap the variables if first argument is larger, so we can iterate in loop properly
+  if (int1 > int2) {
+    let tmp = int1; // NO FANCY TRICKS
+    int1 = int2;
+    int2 = tmp;
+  }
+
+  for (x = int1; x <= int2; x++) {
+    sum += x;
+  }
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
