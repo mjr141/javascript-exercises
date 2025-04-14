e8e641e2a9da8ae4b60429b657b9916f1610176c
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-14 13:14:22 +0100

    use Math.round for converting to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 const convertToCelsius = function(tempC) {
   const factor = 5/9;
   let tempF = ((tempC - 32)*factor);
-  tempF = (Math.ceil(tempF*10) / 10); // convert to 1 decimal place
+  tempF = (Math.round(tempF*10) / 10); // convert to 1 decimal place
   return tempF;
 };
 
 const convertToFahrenheit = function(tempF) {
   const factor = 9/5;
   let tempC = (tempF*factor + 32);
-  tempC = (Math.ceil(tempC*10) / 10);
+  tempC = (Math.round(tempC*10) / 10);
   return tempC;
 };
 
