39a9065ba513af49787a2a322f6df8a5d01fa611
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-15 16:06:35 +0100

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(sequenceCount) {
+  let priorValues = [0,0];
+  let nextValue = 1;
+  sequenceCount = Number(sequenceCount); // function accepts strings
+  sequenceCount = (Math.round(sequenceCount*10))/10; // make sure value is an integer
 
+  if (sequenceCount === 0) {
+    return 0;
+  } else if (sequenceCount < 0) {
+    return 'OOPS'
+  } else {
+    for (i = 1; i <= sequenceCount; i++) {
+      nextValue = priorValues.shift() + nextValue;
+      priorValues.push(nextValue);  
+    }
+  }
+  return nextValue;
 };
 
 // Do not edit below this line
