561f5c237822b8c02d4d1b5d5fe63a5e1a8eaf4c
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-15 15:14:00 +0100

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.replace(/[\W_]/g, "")
+    .toLowerCase();
+  const stringRev = string.split("")
+    .reverse()
+    .join("")
+  return string === stringRev ? true : false;
 };
 
 // Do not edit below this line
