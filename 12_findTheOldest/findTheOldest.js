7901605a92f8de0f07d01a09c13e1f00a14cc8d0
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-16 11:07:57 +0100

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // to get today's year in case people.yearOfDeath is undefined
+  const today = new Date();
 
-};
+  // get an array of the ages to examine
+  const ages = people.map((person) => 
+    (person.yearOfDeath === undefined) ? 
+    today.getFullYear() - person.yearOfBirth : 
+    person.yearOfDeath - person.yearOfBirth);
+
+  // get the index of the oldest person
+  const indexOfOldest = ages.reduce((previousIndex, currentAge, currentIndex, ages) =>
+    currentAge > ages[previousIndex] ? currentIndex : previousIndex, 0);
+
+  return people[indexOfOldest];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
